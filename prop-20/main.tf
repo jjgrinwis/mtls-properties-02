c2088e7ef35131447a08e181c43c570f7d2a3c67
John Grinwis <john@grinwis.com>
2024-12-24 17:19:56 +0100

    moved to module based

--- prop-20/main.tf
+++ prop-20/main.tf
@@ -1,14 +1,5 @@
-# use Terraform Cloud to store our state
-terraform {
-  cloud {
-    organization = "grinwis-com"
-    workspaces {
-      name = "prop-20"
-    }
-  }
-}
-
-# let's create a dummy resource
-resource "terraform_data" "replacement" {
-  input = var.revision
+module "dummy" {
+  source         = "app.terraform.io/grinwis-com/dummy/akamai"
+  version        = "0.0.2"
+  entitlement_id = var.entitlement_id
 }
--- prop-20/outputs.tf
+++ prop-20/outputs.tf
@@ -2,8 +2,3 @@ output "created_hostnames" {
   description = "The created hostnames"
   value       = var.created_hostnames
 }
-
-output "id" {
-  description = "entitlement_id"
-  value       = var.entitlement_id
-}
