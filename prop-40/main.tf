8bbfaf36d78ee115cbe62feaed683c1a7df688b9
John Grinwis <john@grinwis.com>
2024-12-24 18:05:22 +0100

    some manual trigger

--- prop-40/main.tf
+++ prop-40/main.tf
@@ -3,3 +3,35 @@ module "dummy" {
   version        = "0.0.2"
   entitlement_id = var.entitlement_id
 }
+
+resource "null_resource" "trigger_run" {
+  provisioner "local-exec" {
+    command = <<EOT
+      curl -X "POST" "https://app.terraform.io/api/v2/runs" \
+     -H 'Authorization: Bearer ${var.tfe_token}' \
+     -H 'Content-Type: application/vnd.api+json' \
+     -d $'{
+  "data": {
+    "attributes": {
+      "replace-addrs": [],
+      "is-destroy": false,
+      "refresh": true,
+      "refresh-only": false,
+      "allow-config-generation": true,
+      "debugging-mode": false
+    },
+    "relationships": {
+      "workspace": {
+        "data": {
+          "type": "workspaces",
+          "id": "ws-RK7tpcWfxAZkR5Ym"
+        }
+      }
+    },
+    "type": "runs"
+  }
+}'
+
+    EOT
+  }
+}
