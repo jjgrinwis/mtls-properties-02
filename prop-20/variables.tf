229ce6bb66d2d1a03787a9e96e08515838a6bf87
John Grinwis <john@grinwis.com>
2024-12-24 13:16:30 +0100

    update variables.tf

--- prop-20/variables.tf
+++ prop-20/variables.tf
@@ -1,11 +1,16 @@
 variable "revision" {
-    description = "Our version number"
-    type        = number
-    default = 1
+  description = "Our version number"
+  type        = number
+  default     = 1
 }
 
 variable "created_hostnames" {
-    description = "Our version number"
-    type        = list(string)
-    default = []
+  description = "Our version number"
+  type        = list(string)
+  default     = []
+}
+
+variable "entitlement_id" {
+  description = "Our version number"
+  type        = list(string)
 }
