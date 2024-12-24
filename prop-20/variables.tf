bc648030ffc5546d9158031717690e930bd4af98
John Grinwis <john@grinwis.com>
2024-12-24 13:23:40 +0100

    added some extra validation/message to entitlement id

--- prop-20/variables.tf
+++ prop-20/variables.tf
@@ -11,6 +11,12 @@ variable "created_hostnames" {
 }
 
 variable "entitlement_id" {
-  description = "Our version number"
-  type        = list(string)
+  description = "Our CPS certificate entitlement_id"
+  type        = number
+  nullable    = false
+
+  validation {
+    condition     = length(var.entitlement_id) > 0
+    error_message = "Make sure you have a valid CPS entitlement id. So create certificate before trying to make a property. "
+  }
 }
