ed8166ebcae1d485df4764270d0bb66874b7198b
John Grinwis <john@grinwis.com>
2024-12-24 13:07:16 +0100

    test

--- prop-20/outputs.tf
+++ prop-20/outputs.tf
@@ -2,3 +2,8 @@ output "created_hostnames" {
   description = "The created hostnames"
   value       = var.created_hostnames
 }
+
+output "id" {
+  description = "entitlement_id"
+  value       = var.entitlement_id
+}
