c027506a0ef8d8d53424d1414d8f078c5473ed6b
John Grinwis <john@grinwis.com>
2024-12-24 13:32:17 +0100

    output added for prop-30

--- prop-30/variables.tf
+++ prop-30/variables.tf
@@ -9,3 +9,14 @@ variable "created_hostnames" {
   type        = list(string)
   default     = []
 }
+
+variable "entitlement_id" {
+  description = "Our CPS certificate entitlement_id"
+  type        = number
+  nullable    = false
+
+  validation {
+    condition     = var.entitlement_id > 0
+    error_message = "Make sure you have a valid CPS entitlement id. So create certificate before trying to make a property. "
+  }
+}
